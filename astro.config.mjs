a1b5ece59c543d9b5d1d9f93dc7f585a3970ce5a
Nick Hagar <nicholashagar2018@u.northwestern.edu>
2025-05-28 11:10:33 -0500

    initial scaffolding

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+    site: 'https://NHagar.github.io',
+    base: '/',
+
+    integrations: [tailwind(
+        {
+        }
+    )]
+});
